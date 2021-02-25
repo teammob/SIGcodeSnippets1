f3e65f622fddd9b0b107f6ae3af76b0b19be57cc
yucel zengin <39216396+teammob@users.noreply.github.com>
2021-02-25 12:56:10 +0000

    Create Data2.java

---
+++ src/package1/Data2.java
@@ -0,0 +1,24 @@
+package package1;
+
+public class Data2 {
+
+    private int c;
+    private final long id;
+
+    public int getC() {
+        return c;
+    }
+
+    public void setC(int c) {
+        this.c = c;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public Data2(int c, long id) {
+        this.c = c;
+        this.id = id;
+    }
+}
